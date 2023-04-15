06eada8bb27e57df3de6558bdeea4ebae36935b6
PAVAN903 <navap903@gmail.com>
2023-04-15 11:18:33 +0530

    Added:multiple ride total fare

--- src/com/bridgelab/CabInvoiceGenerator.java
+++ src/com/bridgelab/CabInvoiceGenerator.java
@@ -3,8 +3,9 @@ package com.bridgelab;
 public class CabInvoiceGenerator {
 	double fareperKm=10.0;
 	double minFare=5.0;
+	double totalfare=0;
 	public double calculateFare(double distance, double timeInminute) {
-		double totalfare=(distance*fareperKm)+(timeInminute)+ minFare;
+     totalfare=totalfare+(distance*fareperKm)+(timeInminute)+ minFare;
 		return totalfare;
 	}
 
--- src/com/bridgelab/Main.java
+++ src/com/bridgelab/Main.java
@@ -3,15 +3,20 @@ package com.bridgelab;
 import java.util.Scanner;
 
 public class Main {
+	 static double totalfare=0;
 
 	public static void main(String[] args) {
 		CabInvoiceGenerator cabinvoice=new CabInvoiceGenerator();
 		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number of rides");
+		int rides=sc.nextInt();
+		for(int i=0;i<rides;i++) {
 		System.out.println("enter distance travelled");
 		double distance=sc.nextDouble();
 		System.out.println("enter the time travelled");
 		double timeInminute=sc.nextDouble();
-		double totalfare=cabinvoice.calculateFare(distance, timeInminute);
+		 totalfare=cabinvoice.calculateFare(distance, timeInminute);
+		}
 		System.out.println("Total fare is : "+totalfare);
 	}
 
